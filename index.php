561757cc01ccdfdcf52594c6cc25a161b130411a
AwThuCho <blackhole4614@gmail.com>
2024-11-21 14:36:41 +0630

    preschool php project succeeded

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+include('./layouts/header.php');
+include('./layouts/nav.php');
+include('./layouts/spinner.php');
+include('./layouts/carousel.php');
+
+?>
+
+<section id="about">
+    <?php
+    include('./layouts/about.php');
+    ?>
+
+</section>
+
+<section id="classes">
+    <?php
+    include('./layouts/class.php');
+    ?>
+
+</section>
+
+<section id="facility">
+    <?php
+    include('./layouts/facility.php');
+    ?>
+
+</section>
+
+<section id="team">
+    <?php
+    include('./layouts/team.php');
+    ?>
+
+</section>
+
+<section id="call-to-action">
+    <?php
+    include('./layouts/class.php');
+    ?>
+
+</section>
+
+<section id="appointment">
+    <?php
+    include('./layouts/appointment.php');
+    ?>
+
+</section>
+
+<section id="testimonial">
+    <?php
+    include('./layouts/testimonial.php');
+    ?>
+
+</section>
+
+<section id="contact">
+    <?php
+    include('./layouts/contact.php');
+    ?>
+
+</section>
+
+
+
+<?php
+include('./layouts/footer.php')
+
+?>
